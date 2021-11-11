81b0a3cf42577f98fe10ddc2a2cde9681f091527
Mohamed Ibrahim <mohamed.ibrahim02023@gmail.com>
2021-11-12 01:02:11 +0200

    Adding debug main introsort

--- introsort.cpp
+++ introsort.cpp
@@ -24,3 +24,12 @@ void heapsort(vector<int>::iterator begin, vector<int>::iterator end)
 void introsort(vector<int> a, vector<int>::iterator begin, vector<int>::iterator end, int max_depth){
     
 }
+
+#if DEBUG_INTRO_SORT
+int main(){
+    std::vector<int> nums{-1,5,7,2,3,4,0,17,0,0,17};
+    introsort(nums, begin(nums), end(nums));
+    for(auto num : nums)
+        cout << num << std::endl;
+}
+#endif
